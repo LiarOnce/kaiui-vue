62c22971159cef5feed690e3658843b2fbe57317
Jakub Janek <jakubjanek@protonmail.com>
2020-04-25 01:19:42 +0200

    Moving into one source directory, removed jsx, url and path fixes, removed node-sass dependency

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="app">
+		<Header>Hello there</Header>
+		<TextInput label="Textové pole"></TextInput>
+		<ArrowListItem primary="Oof" secondary="Hello"></ArrowListItem>
+		<Button icon-side="right"></Button>
+	</div>
+</template>
+
+<script>
+    import Header from "./components/Header/Header";
+    import TextInput from "./components/TextInput/TextInput";
+    import ArrowListItem from "./components/ArrowListItem/ArrowListItem";
+    import Button from "./components/Button/Button";
+
+    export default {
+        name: 'App',
+        components: {
+            Header,
+						TextInput,
+						ArrowListItem,
+						Button
+        }
+    }
+</script>
+
+<style>
+
+</style>
